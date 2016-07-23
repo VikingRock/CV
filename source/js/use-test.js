d0dcf99bd178ea353cc30934930aa9a0fdc33d01
VikingRock <a.zinchuk.a@gmail.com>
2016-07-23 23:36:32 +0300

    menu mobile + desktop, initial js

--- source/js/use-test.js
+++ source/js/use-test.js
@@ -3,3 +3,13 @@
 var Test = require('test');
 
 console.log(Test);
+
+var btnHamburger = document.querySelector('.main-nav__menu');
+var mobileMenu = document.querySelector('.main-nav ul');
+
+var toggleVisible = function() {
+  mobileMenu.classList.toggle('invisible');
+};
+
+btnHamburger.addEventListener('click', toggleVisible);
+

373da8c0a909d7bc62e55f35454dc19e9c801948
VikingRock <a.zinchuk.a@gmail.com>
2016-07-24 23:16:24 +0300

    top menu js added, scroll to top added

--- source/js/use-test.js
+++ source/js/use-test.js
@@ -2,14 +2,74 @@
 
 var Test = require('test');
 
-console.log(Test);
-
 var btnHamburger = document.querySelector('.main-nav__menu');
 var mobileMenu = document.querySelector('.main-nav ul');
+var scrollTopBtn = document.querySelector('.scroll-top');
+var topMenuHeight = 100;
+var scrollTimeout;
 
 var toggleVisible = function() {
   mobileMenu.classList.toggle('invisible');
 };
 
+/**
+ * Function shows or hides the Scroll To Top button
+ * depending on the page scroll
+ */
+var scrollTopToggle = function() {
+
+  clearTimeout(scrollTimeout);
+
+  scrollTimeout = setTimeout( function() {
+    if (window.scrollY > 100) {
+      scrollTopBtn.classList.remove('invisible');
+    } else {
+      scrollTopBtn.classList.add('invisible');
+    }
+  }, 100);
+};
+
+/**
+ * Function smoothly scrolls the @element to @to
+ * @param {Element} element
+ * @param {Element} to
+ * @param {number} duration
+ */
+var scrollTo = function (element, to, duration) {
+  if (duration <= 0) return;
+  var difference = to - element.scrollTop;
+  var perTick = difference / duration * 10;
+
+  setTimeout(function() {
+    element.scrollTop = element.scrollTop + perTick;
+    if (element.scrollTop === to) return;
+    scrollTo(element, to, duration - 10);
+  }, 10);
+};
+
+var scrollToAnchor = function(event) {
+  event.preventDefault();
+  var target = event.target;
+  if (target.tagName.toUpperCase() !== 'A') return;
+
+  var anchor = target.getAttribute('href');
+  anchor = anchor.substr(1);
+  var to = document.getElementById(anchor);
+
+  //hack to make this shit work in FF
+  if (document.documentElement.scrollTop++) {
+    var element = document.documentElement
+  } else {
+    var element = document.body;
+  }
+
+  scrollTo(element, to.offsetTop - topMenuHeight, 900);
+  mobileMenu.classList.add('invisible');
+};
+
 btnHamburger.addEventListener('click', toggleVisible);
 
+window.addEventListener('scroll', scrollTopToggle);
+
+mobileMenu.addEventListener('click', scrollToAnchor);
+scrollTopBtn.addEventListener('click', scrollToAnchor);

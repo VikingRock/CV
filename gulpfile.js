6ecd4113b90f91ecdd94b066511061977f326379
VikingRock <a.zinchuk.a@gmail.com>
2016-07-30 14:29:06 +0300

    reload added on gulp command after images are processed

--- gulpfile.js
+++ gulpfile.js
@@ -15,14 +15,6 @@ var gulp = require('gulp'),
     webpack = require('webpack-stream'),
     clean = require('gulp-clean');
 
-var jsLibs = [
-  /*'./bower_components/moment/min/moment-with-locales.min.js',
-  './bower_components/tap/dist/tap.min.js',
-  './bower_components/mustache.js/mustache.min.js',*/
-  './source/js/modules/*.js',
-  './source/js/main.js'
-];
-
 //clean build
 gulp.task('clean', function () {
   return gulp.src('./build/*', {read: false})
@@ -52,7 +44,7 @@ gulp.task('connect', function() {
 gulp.task('html', function () {
   gulp.src('./source/*.html')
     .pipe(gulp.dest('./build/'));
- gulp.src('./build/*.html')
+  gulp.src('./build/*.html')
     .pipe(connect.reload());
 });
 
@@ -84,9 +76,16 @@ gulp.task('scripts', function() {
 
 //images
 gulp.task('images', function() {
-  gulp.src('source/img/*')
+  gulp.src('./source/img/*')
     .pipe(imagemin())
-    .pipe(gulp.dest('build/img'))
+    .pipe(gulp.dest('./build/img/'));
+
+  setTimeout(function() {
+    gulp.src('./build/img/*')
+      .pipe(connect.reload());
+    console.log('==============================\npage reloaded' +
+      '\n==============================');
+  }, 7000);
 });
 
 // watch
@@ -103,8 +102,8 @@ gulp.task('default', function() {
     'copy',
     'style',
     'scripts',
-    'images',
     'connect',
+    'images',
     'watch'
   );
 });
